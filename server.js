2c3fb337b9a222581fe3c302c65fb3e6e61c0d3e
sebusch <sebusch@gmail.com>
2016-10-29 14:56:33 +1100

    working version

--- server.js
+++ server.js
@@ -1,13 +1,61 @@
 var express = require( 'express' );
+var favicon = require( 'serve-favicon' );
 
 var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use( favicon( './favicon.ico' ) );
 
-app.listen(port, function () {
-  console.log('Example app listening on port 8080!');
-});
+app.set( 'view options', {
+  layout: false
+} );
+app.use( express.static( __dirname + '/public' ) );
+
+app.use( '/:input', function( req, res ) {
+
+  res.send( processInput( req.params.input ) );
+} );
+
+app.use( '/', function( req, res, next ) {
+  //res.send( 'Hello everyone' );
+  res.render( 'index.html' );
+} );
+
+
+app.use( function( err, req, res, next ) {
+  res.sendStatus( 400 ).send( {
+    error: 'malformed url'
+  } );
+  next( err );
+} );
+app.use( function( err, req, res, next ) {
+  console.log( err );
+} )
+
+
+app.listen( port, function() {
+  console.log( 'Example app listening on port %d!', port );
+} );
+
+function processInput( input ) {
+  var date = ( +input ) ? new Date( +input ) : new Date( input );
+  var output = {
+    unix: null,
+    natural: null
+  };
+  if ( date.getDate() ) {
+    output.unix = date.getTime();
+    output.natural = dateParser( date );
+  }
+  return output;
+}
+
+function dateParser( date ) {
+  var options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+  return date.toLocaleDateString( 'en-US', options );
+}
